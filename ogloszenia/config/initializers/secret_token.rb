c627dd0c4dbb5ac22584b6c6488c46b3c25700d4
Wojtek Radlak <pingwidon@gmail.com>
2011-03-27 15:05:48 +0200

    utworzyłem rusztowanie

---
+++ ogloszenia/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ogloszenia::Application.config.secret_token = '60d504c9dc982ceba85c47bf4cb466af5605bb1511cd4f139fef97043ea920da8b579c0f286a550f30c9a60018a10d29bab4bf65d283742279d9a6f245ba9a04'
